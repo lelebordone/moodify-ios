c317d20f61c854c6336c6df4de1710e23bee5384
Emanuele Bordone <e.bordone1997@gmail.com>
2020-10-31 10:34:23 +0100

    Initial Commit

---
+++ Moodify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moodify
+//
+//  Created by Emanuele Bordone on 31/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moodify/MoodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodifyApp.swift
+//  Moodify
+//
+//  Created by Emanuele Bordone on 31/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MoodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
